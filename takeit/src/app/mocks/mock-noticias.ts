9b2cd18fbf5443bcdf5a9536be9510ef25beba94
Fabricio Herrera <jfherrer@espol.edu.ec>
2019-07-09 18:34:50 -0500

    Anadiendo sección de noticias

---
+++ takeit/src/app/mocks/mock-noticias.ts
@@ -0,0 +1,18 @@
+import { Noticias } from '../models/noticias';
+
+export const NOTICIAS: Noticias[] =  [
+    {
+        id: 1,
+        img_url: "https://scontent.fuio13-1.fna.fbcdn.net/v/t1.0-9/60749849_2387888081276107_7877238353240784896_o.jpg?_nc_cat=108&_nc_oc=AQk4UNHdY9TBuk5JkbuY1luSajyzT-XteRm56eWHeFuU9I2FNWB9FHzIVZY4fbbV0NM&_nc_ht=scontent.fuio13-1.fna&oh=c12d769ec9c4cd13e7254aee226d23b4&oe=5DBD4AE8",
+        titulo: "Festín carnívoro; rincón imperdible",
+        descripcion: "Crítico de gastronomía visita Rincón Rústiko, un lugar de carnes a la parrilla y mucho más",
+        url_noticia: "https://www.eluniverso.com/larevista/2019/07/07/nota/7409520/festin-carnivoro-rincon-imperdible",
+    },
+    {
+        id: 2,
+        img_url: "https://www.eluniverso.com/sites/default/files/styles/powgallery_1280/public/fotos/2019/07/17681131.jpg?itok=SCAdqTUW",
+        titulo: "20 variedades de platos con langosta en festival de restaurante El Fortín",
+        descripcion: "“En un festival pueden llegar a prepararse más de 2.200 langostas de este tipo, en toda la cita gastronómica, de ahí también piden la langosta al grill, la langosta en salsa de jengibre con coco, paellas de langosta, ensaladas, cebiches, una serie de platos”, detalla.",
+        url_noticia: "https://www.eluniverso.com/entretenimiento/2019/07/06/nota/7411383/festival-langosta-alternativa-consentir-paladar",
+    }
+]
