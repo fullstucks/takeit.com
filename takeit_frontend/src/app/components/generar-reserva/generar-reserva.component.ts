10d583a1f50c209486cd2d9b43796eb9367960bf
Neythan León <neythan.astro@gmail.com>
2019-09-04 16:05:50 -0500

    Actualizacion de vistas para reserva e implementacion de proceso para reserva con api

--- takeit_frontend/src/app/components/generar-reserva/generar-reserva.component.ts
+++ takeit_frontend/src/app/components/generar-reserva/generar-reserva.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { TakeitdataService } from 'src/app/services/takeitdata.service';
 import { Location } from '@angular/common';
 import { Restaurant } from 'src/app/models/restaurant';
+import { Planificados } from 'src/app/models/planificaciones';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-generar-reserva',
@@ -10,15 +12,41 @@ import { Restaurant } from 'src/app/models/restaurant';
   styleUrls: ['./generar-reserva.component.css']
 })
 export class GenerarReservaComponent implements OnInit {
+  
+  stepperForm: FormGroup;
+  actualFieldForm;
+  nextFieldForm;
+  previousFieldForm;
+ 
+  isEditable = false;
+  isCollapsedEntradas:boolean=false;
+  isCollapsedConfirmacion: boolean=true;
+  isCollapsedPago: boolean= true;
 
-  restaurant: Restaurant
+  restaurant: any;
+  planificados:Planificados[];
+  fechasPlanificadas:[];
 
+  planSelecionado:Planificados;
+  
+  mesasDisponibles:any;
+  verSeleccion:any="";
+  objetoSeleccionado:any="0";
+
+  step1:String = "active"; step2: String; step3: String;
+
+  elegido:string="";
+  formVisibility:boolean;
   constructor(private route: ActivatedRoute,
               private location: Location,
-              private takeitDataService: TakeitdataService) { }
+              private takeitDataService: TakeitdataService, 
+             ) { }
 
   ngOnInit() {
+
     this.getRestaurant()
+    this.getPlanificados()
+    
   }
 
 
@@ -28,10 +56,64 @@ export class GenerarReservaComponent implements OnInit {
   getRestaurant():void{
     const id =+ this.route.snapshot.paramMap.get('id');
     this.takeitDataService.getRestaurant(id)
-        .subscribe(restaurant => this.restaurant = restaurant)
+        .subscribe(restaurant => {
+          this.restaurant = restaurant
+          console.log(this.restaurant)
+        })
   }
 
+  
+  getPlanificados():void{
+    const id =+ this.route.snapshot.paramMap.get('id');
+    this.takeitDataService.getReservasPlanificadas(id)
+       .subscribe(
+         data=>{
+           this.planificados= data;
+           var line = [];
+           for (var i = 0; i < data.length; i++) {
+            var datafecha = data[i].fecha.split(' ');
+            var linefecha;
+            linefecha[0] = datafecha[0];
+            linefecha[1] = datafecha[1];
+            line.push(linefecha)  
+          } 
+          console.log(line)      
+          console.log(this.planificados)
+         }
+       )
+  }
+  onChange(plan){
+    this.verSeleccion=plan;
+    console.log(this.verSeleccion)
+  }
+  
   goBack(): void {
     this.location.back();
   }
+
+  goStep1(): void{
+    this.isCollapsedEntradas =false;
+    this.isCollapsedConfirmacion=true;
+    this.isCollapsedPago=true;
+    this.step1="active";
+    this.step2="0";
+    this.step3="0";
+  }
+  goStep2(): void {
+    this.isCollapsedEntradas =true;
+    this.isCollapsedConfirmacion=false;
+    this.isCollapsedPago=true;
+    this.step1="active";
+    this.step2="active";
+    this.step3="0";
+  }
+  goStep3(): void {
+    this.isCollapsedEntradas =true;
+    this.isCollapsedConfirmacion=true;
+    this.isCollapsedPago=false;
+    this.step1="active";
+    this.step2="active";
+    this.step3="active";
+  }
+ 
 }
---
+++ takeit_frontend/src/app/models/reservaplan.ts
@@ -0,0 +1,6 @@
+export class ReservaPlan{
+    id_restaurant: number;
+    mesas_totales: boolean;
+    mesas_disponibles: string;
+    fecha: string;
+}
