850f2bf3de5ae0fa085377f19750d35f15da7793
miguelquo <miguelps95@gmail.com>
2019-09-04 16:26:26 -0500

    add admnistrarReservas component

---
+++ takeit_frontend/src/app/components/administrar-reservas/administrar-reservas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-administrar-reservas',
+  templateUrl: './administrar-reservas.component.html',
+  styleUrls: ['./administrar-reservas.component.css']
+})
+export class AdministrarReservasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
